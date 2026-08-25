cd195f107d17f11daa45ffaf6bd42714e628d3ef
Deepesh Kafaltiya <kafaltiyadeepesh@gmail.com>
2026-08-25 13:39:02 +0530

    Initial commit for new repository

--- src/data/sponsorsData.js
+++
@@ -1,5 +0,0 @@
-export const SPONSORS_DATA = [
-  { id: 'sp-1', name: 'Google Cloud', tier: 'Title', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg', website: 'https://cloud.google.com' },
-  { id: 'sp-2', name: 'GitHub', tier: 'Gold', logo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg', website: 'https://github.com' },
-  { id: 'sp-3', name: 'Devfolio', tier: 'Community', logo: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg', website: 'https://devfolio.co' }
-];
